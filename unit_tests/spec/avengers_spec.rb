76b98fb833bd0d61474ab5f765ae89223074c7ea
vgdcarvalho <vgdcarvalho@gmail.com>
2019-06-10 17:21:41 -0300

    Finalizando avengers_spec.rb

--- unit_tests/spec/avengers_spec.rb
+++ unit_tests/spec/avengers_spec.rb
@@ -46,4 +46,37 @@ describe AvengersHeadQuarter do
         result = hq.list.size > 0
         expect(result).to be true
     end
+
+    #ver se começa com determinado item
+    it 'Thor deve ser o primeiro da lista' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Ironman')
+        expect(hq.list).to start_with('Thor')
+    end
+
+    #ver se termina com determinado item
+    it 'Ironman deve ser o último da lista' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Ironman')
+        expect(hq.list).to end_with('Ironman')
+    end
+
+    #ver se contem determinada regex
+    it 'deve conter o sobrenome' do
+        avenger = 'Peter Parker'
+
+        expect(avenger).to match(/Parker/)
+    end
+
+    #ver se NÃO contem determinada regex
+    it 'deve conter o sobrenome' do
+        avenger = 'Peter Parker'
+
+        expect(avenger).not_to match(/Carvalho/)
+    end
+
 end
