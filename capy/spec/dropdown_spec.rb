4d682d0fce23fcf8b81fa843ec17ad8019b981ad
Vinicius Carvalho <vgdcarvalho@gmail.com>
2019-09-24 14:08:13 -0300

    Adding dropbox test

---
+++ capy/spec/dropdown_spec.rb
@@ -0,0 +1,27 @@
+describe 'Caixa de seleção', :dropdown do
+
+    it 'selecionar item' do
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+
+        select('Loki', from: 'dropdown') # só dá pra usar se tiver o id setado lá no html
+        sleep 3
+    end
+
+    # isso aqui é pro caso de não ter o id do elemento pra usar
+    it 'selecionar item com find' do
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+
+        drop = find('.avenger-list') # busca elementos da classe "avenger-list"
+        drop.find('option', text: 'Tony Stark').select_option # seleciona uma opção do elemento da classe "avenger-list"
+        sleep 3
+    end
+
+    it 'selecionar qualquer item', :sample do
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+
+        drop = find('.avenger-list')
+        drop.all('option').sample.select_option # escolhe um, aleatoriamente, da lista de itens retornados
+        sleep 3
+    end
+
+end
--- capy/spec/forms_spec.rb
+++ capy/spec/forms_spec.rb
@@ -18,4 +18,27 @@ describe 'Forms' do
         # vê se o elemento tem o texto
         expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
+
+    it 'senha incorreta' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with: 'invalid'
+
+        click_button 'Login'
+
+        expect(find('#flash')).to have_content 'Senha é invalida!'
+    end
+
+    it 'usuário não cadastrado' do
+        visit 'https://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'steve'
+        fill_in 'password', with: 'jarvis!'
+
+        click_button 'Login'
+
+        expect(find('#flash')).to have_content 'O usuário informado não está cadastrado!'
+    end
+
 end
