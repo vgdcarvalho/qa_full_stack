c6a9d899fb73e3a02c3aee5d4a9e5889f61bf168
Vinicius Carvalho <vinicius.carvalho@vistajet.com>
2020-08-13 20:57:37 -0300

    Adicionando aula de variaveis JS

--- js4t/main.js
+++ js4t/main.js
@@ -1,3 +1,4 @@
 //Usando um arquivo
 var welcome3 = "Hello world! - 4"
 document.getElementById("welcome3").append(welcome3)
+
---
+++ js4t/variaveis.js
@@ -0,0 +1,15 @@
+//variaveis
+var number = 10;
+console.log(number)
+
+//pegar tipo da variavel
+console.log(typeof number)
+
+var name = "Tester Test"
+console.log(typeof name)
+
+//operações no print
+var n1 = 10
+var n2 = 20
+
+console.log(n1 + n2)
